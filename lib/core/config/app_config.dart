8d7d399f0bf86b01cffe085e3e7989b37f82b0a2
hassanein313 <133112955+hassanein313@users.noreply.github.com>
2026-08-19 11:44:49 +0300

    إصلاح حلقة التحديث المتكرر ومزامنة AppConfig.appBuildNumber = 15 مع APK الإصدار 1.2.5

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -25,8 +25,8 @@ abstract final class AppConfig {
   static const currency = 'د.ع';
 
   /// معلومات الإصدار الموحّدة
-  static const appVersion = '1.2.4';
-  static const appBuildNumber = 14;
+  static const appVersion = '1.2.5';
+  static const appBuildNumber = 15;
   static String get versionDisplay => 'الإصدار $appVersion (Build $appBuildNumber)';
 
   /// أسماء صناديق Hive المحلية
